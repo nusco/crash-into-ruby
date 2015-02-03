27357a739b19f443162a8127c461805eca487622
Paolo "Nusco" Perrotta <paolo.nusco.perrotta@gmail.com>
2015-02-04 00:07:57 +1100

    Complete handson 5 (Singleton Classes)

--- handson/handson_5_singleton_classes.rb
+++ handson/handson_5_singleton_classes.rb
@@ -1,2 +1,17 @@
-# TODO: show how to open a singleton class
-# both with "class <<" and with the new singleton_class method
+class MyClass; end
+
+MyClass.singleton_class # => #<Class:MyClass>
+String.singleton_class  # => #<Class:String>
+"abc".singleton_class   # => #<Class:#<String:0x007fa29120dad8>>
+
+class << MyClass
+  self   # => #<Class:MyClass>
+end
+
+class << MyClass
+  def a_class_method
+    "OK!"
+  end
+end
+
+MyClass.a_class_method  # => "OK!"
