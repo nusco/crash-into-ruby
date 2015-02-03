eeb29ba3b66d573dfc2bea0b37df672444c82da8
Paolo "Nusco" Perrotta <paolo.nusco.perrotta@gmail.com>
2015-02-03 22:45:03 +1100

    Fix comments in Tangle of Modules

--- quizzes/tangle_of_modules/tangle_of_modules.rb
+++ quizzes/tangle_of_modules/tangle_of_modules.rb
@@ -1,17 +1,24 @@
+# In this quiz, you're not going to write (or even run) code until the very last
+# moment. You're supposed to reason about this and discuss it with your pair.
+#
 # Your company asked you to fix a bug in its legacy printing system. According
-# to the company’s bug management application, there is a problem with the code below:
-# print_to_screen() is not calling the right print() method. The bug report doesn’t
-# provide anymore details.
+# to the company’s bug tracker, there is a problem with the code below. The bug
+# description is simply:
+#
+# "The print_to_screen() method is not calling the right print() method."
+#
+# ...and that's it. The bug report doesn’t provide anymore details.
 #
 # You can just run the code and see what's happening - and after that, you can fix the
 # bug by changing a single line. But don't do that just yet. Instead, try to reason about
 # the code. To solve this quiz, do two things:
 #
-# 1. Draw the chain of modules on paper. Based on that, can you guess which version of
-# print() gets called—the one in Printable or the one in Document?
+# 1. Draw the chain of ancestors of the Book class on paper. Based on that, can you
+# guess which version of print() gets called — the one in Printable, or the one in
+# Document?
 #
-# 2. How can you quickly fix the code so that print_to_screen() calls the other version
-# of print() instead?
+# 2. Think about the quickest way to fix the code so that print_to_screen() calls the
+# other version of print() instead.
 #
 # Try to do both things before you ever run this program. Then feel free to check whether
 # your assumption are right: run the program, see what happens, and finally fix the code.
