6eb234ac986b6fffc0a36017098004612ecc23f7
Paolo "Nusco" Perrotta <paolo.nusco.perrotta@gmail.com>
2015-02-03 17:43:54 +1100

    Tweak handson 2

--- handson/handson_2_blocks.rb
+++ handson/handson_2_blocks.rb
@@ -1,12 +1,17 @@
+for i in 1..5
+  puts i
+end
+
 # for loops are rarely ever used. instead...
 
 # blocks
 3.times { puts "hi" }
-2.upto(4) {|i| puts "#{i}..." }
-3.upto(5) do |i|
+2.upto(4) {|i| puts "#{i}..." } # !> shadowing outer local variable - i
+3.upto(5) do |i| # !> shadowing outer local variable - i
   puts "#{i}..."
 end
 
+stuff = ["hello", "world"]
 stuff.each do |e|
   puts e.reverse
 end
@@ -23,12 +28,18 @@ def decorate
 end
 decorate { puts "...block..." }
 
-def callback
+def callback # !> previous definition of callback was here
   yield 3
 end
 callback {|n| n * 2 }
 
-def callback
+def callback # !> method redefined; discarding old callback
   yield 3, 2
 end
 callback {|n, m| n * m }
+
+def store_and_call(a, b, &c)
+  callable_thing = c
+  callable_thing.call(a, b)
+end
+store_and_call(1, 2) {|x, y| x + y } # => 3
